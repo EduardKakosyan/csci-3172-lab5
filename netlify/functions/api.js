5be4536c9e0e0189d6f96e0d28f177cdc3c8c634
EduardKakosyan <kakosyaneduard@dal.ca>
2025-03-09 11:53:56 -0300

    [Finish] Lab 5

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// API Routes - Note the base path change
+// API Routes
 app.use('/api/recipes', recipeRoutes);
 
 // Error handling middleware
